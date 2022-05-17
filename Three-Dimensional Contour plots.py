ec7119c8b45166bcb2e8e7adfbeeab476e111161
Reza Ebrahimi <78690714+Reza-Ebrahimi2020@users.noreply.github.com>
2022-05-17 17:41:15 +0500

    Create Three-Dimensional Contour plots.py

---
+++ Three-Dimensional Contour plots.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 17 17:35:29 2022
+
+@author: Reza
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+def f(x,y):
+    return np.sin(np.sqrt(x**2+y**2))
+x = np.linspace(-6,6,30)
+y = np.linspace(-6,6,30)
+x,y = np.meshgrid(x,y)
+z = f(x,y)
+fig = plt.figure()
+ax = plt.axes(projection = '3d')
+ax.contour3D(x,y,z,50, cmap = 'binary')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+plt.show()
