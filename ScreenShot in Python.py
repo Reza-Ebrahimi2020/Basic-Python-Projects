0ebdef4264d953836b569c2dd1d654651d166c6b
Reza Ebrahimi <78690714+Reza-Ebrahimi2020@users.noreply.github.com>
2022-05-19 08:50:10 +0500

    Create ScreenShot in Python.py

---
+++ ScreenShot in Python.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 19 08:45:20 2022
+
+@author: Reza
+"""
+
+import pyautogui
+
+myscreenshot = pyautogui.screenshot()
+
+myscreenshot.save(r'E:online.png')
